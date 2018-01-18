81420da03d4f56b4fdc87b71a2b0666863e9a0e1
Aidan Steele <aidan.steele@glassechidna.com.au>
2018-01-18 17:31:52 +1100

    Added LambdaMiddleware wrapper class

---
+++ HttpClientMiddleware/src/LambdaMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientMiddleware
+{
+    public class LambdaMiddleware: IMiddleware
+    {
+        private readonly Func<HttpRequestMessage, Func<HttpRequestMessage, Task<HttpResponseMessage>>, Task<HttpResponseMessage>> _func;
+
+        public LambdaMiddleware(Func<HttpRequestMessage, Func<HttpRequestMessage, Task<HttpResponseMessage>>,Task<HttpResponseMessage>> func)
+        {
+            _func = func;
+        }
+
+        public Task<HttpResponseMessage> Invoke(HttpRequestMessage request, Func<HttpRequestMessage, Task<HttpResponseMessage>> next)
+        {
+            return _func(request, next);
+        }
+    }
+}
