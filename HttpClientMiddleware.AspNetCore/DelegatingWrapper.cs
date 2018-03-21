df668758a2e00e0331977ec8df14315750b93a0c
Aidan Steele <aidan.steele@glassechidna.com.au>
2018-03-21 15:57:56 +1100

    DelegatingWrapper should *not* be terminal, but keep passing down the middleware pipeline

--- HttpClientMiddleware.AspNetCore/DelegatingWrapper.cs
+++ HttpClientMiddleware.AspNetCore/DelegatingWrapper.cs
@@ -8,18 +8,43 @@ namespace HttpClientMiddleware.AspNetCore
 {
     delegate Task<HttpResponseMessage> SendAsyncDelegate(HttpRequestMessage request, CancellationToken cancellationToken);
 
+    class DelegatingHandlerCallbacker : DelegatingHandler
+    {
+        private readonly SendAsyncDelegate _delegate;
+
+        public DelegatingHandlerCallbacker(SendAsyncDelegate dele)
+        {
+            _delegate = dele;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return _delegate(request, cancellationToken);
+        }
+    }
+    
     class DelegatingWrapper : IMiddleware
     {
-        private SendAsyncDelegate _inner;
+        private readonly SendAsyncDelegate _inner;
+        private readonly AsyncLocal<Func<HttpRequestMessage, Task<HttpResponseMessage>>> _next;
 
         public DelegatingWrapper(DelegatingHandler handler)
         {
-            MethodInfo dynMethod = handler.GetType().GetMethod("SendAsync", BindingFlags.NonPublic | BindingFlags.Instance);
+            _next = new AsyncLocal<Func<HttpRequestMessage, Task<HttpResponseMessage>>>();
+            handler.InnerHandler = new DelegatingHandlerCallbacker(Callback);
+            
+            var dynMethod = handler.GetType().GetMethod("SendAsync", BindingFlags.NonPublic | BindingFlags.Instance);
             _inner = (SendAsyncDelegate)dynMethod.CreateDelegate(typeof(SendAsyncDelegate), handler);
+        }
+
+        private Task<HttpResponseMessage> Callback(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return _next.Value(request);
         }
 
         public Task<HttpResponseMessage> Invoke(HttpRequestMessage request, Func<HttpRequestMessage, Task<HttpResponseMessage>> next)
         {
+            _next.Value = next;
             return _inner(request, CancellationToken.None);
         }
     }
